c5f4a25b93cf881bd0cf1e74d203d3bf83bb3d17
guilhermefq <gfantuci@gmail.com>
2017-11-24 09:12:02 -0300

    Aula 4

--- app/src/main/java/br/com/softgran/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/softgran/agenda/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package br.com.softgran.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Daniel", "Ronaldo", "Guilherme", "Lucas", "Rodrigo"};
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, alunos);
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+
+        listaAlunos.setAdapter(adapter);
     }
 }
