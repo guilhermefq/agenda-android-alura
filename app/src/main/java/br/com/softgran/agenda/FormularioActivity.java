8374fc291e143fcf5b1126985cf6b58b6334ee8c
guilhermefq <gfantuci@gmail.com>
2017-11-27 10:58:04 -0300

    Aula 4

--- app/src/main/java/br/com/softgran/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/softgran/agenda/FormularioActivity.java
@@ -2,25 +2,47 @@ package br.com.softgran.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import br.com.softgran.agenda.dao.AlunoDAO;
+import br.com.softgran.agenda.modelo.Aluno;
+
 public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        this.helper = new FormularioHelper(this);
+    }
 
-        Button botaosalvar = (Button) findViewById(R.id.formulario_salvar);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-        botaosalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(FormularioActivity.this,
-                        "Aluno salvo com sucesso!", Toast.LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
+                Aluno aluno = helper.pegaAluno();
+
+                AlunoDAO dao = new AlunoDAO(this);
+                dao.insere(aluno);
+                dao.close();
+
+                //Toast.makeText(FormularioActivity.this,
+                        //"Aluno " + aluno.getNome() + " salvo", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/br/com/softgran/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/softgran/agenda/ListaAlunosActivity.java
@@ -1,9 +1,17 @@
 package br.com.softgran.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+
+import java.util.List;
+
+import br.com.softgran.agenda.dao.AlunoDAO;
+import br.com.softgran.agenda.modelo.Aluno;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -12,11 +20,30 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        String[] alunos = {"Daniel", "Ronaldo", "Guilherme", "Lucas", "Rodrigo"};
+        Button novoAluno = (Button) findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, alunos);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregaLista();
+    }
+
+    public void carregaLista() {
+        AlunoDAO dao = new AlunoDAO(this);
+        List<Aluno> alunos = dao.buscaAlunos();
+        dao.close();
+
         ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<Aluno> adapter =
+                new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, alunos);
 
         listaAlunos.setAdapter(adapter);
     }
